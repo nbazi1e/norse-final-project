60afd18ab8ff6ccdcdc9deb0c6caf357ed8a20cc
narme <narme@thenarmtop.frontierlocal.net>
2018-05-22 00:16:50 -0400

    Created Hello World example

---
+++ src/masterapp.java
@@ -0,0 +1,67 @@
+//Authors: Narmer Bazile & Suhaas N.
+
+
+
+import com.googlecode.lanterna.SGR;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+
+public class masterapp
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException 
+	{
+		//initialize and create terminal window
+		DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+		Terminal terminal = null;
+		
+		try 
+		{
+		
+		//start terminal window
+		terminal = new DefaultTerminalFactory().createTerminal();
+
+		Thread.sleep(5000);
+		terminal.putCharacter('H');
+        terminal.putCharacter('e');
+        terminal.putCharacter('l');
+        terminal.putCharacter('l');
+        terminal.putCharacter('o');
+        terminal.putCharacter('\n');
+        terminal.flush();
+        Thread.sleep(5000);
+		}
+		
+		catch(IOException e) 
+		{
+			
+			e.printStackTrace();
+		}
+		
+		finally 
+		{
+            if(terminal != null) 
+            {
+                try 
+                {
+                    terminal.close();
+                }
+                catch(IOException e) 
+                {
+                    e.printStackTrace();
+                }
+		
+            }
+
+		}
+	}
+}
