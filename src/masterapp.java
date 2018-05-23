3eeb47fa5d855073bb3be13ed206cd3064e35ce8
nbazi1le <narme@10.130.3.201>
2018-05-23 08:44:06 -0400

    Removed unnecessary testing elements and added loading message. Removed utils class, since deemed unnecessary

--- src/masterapp.java
+++ src/masterapp.java
@@ -1,56 +1,50 @@
 //Authors: Narmer Bazile & Suhaas N.
 
-
-
 import com.googlecode.lanterna.SGR;
-
-import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.TerminalResizeListener;
+
 
 import java.io.IOException;
-
 
 public class masterapp
 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws InterruptedException 
 	{
 		//initialize and create terminal window
 		DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
 		Terminal terminal = null;
+
 		
 		try 
 		{
 		
 		//start terminal window
 		terminal = new DefaultTerminalFactory().createTerminal();
-
+		
+		final TextGraphics loadMessage = terminal.newTextGraphics();
+		loadMessage.putString(1,1, "Loading ...", SGR.BOLD);
+		terminal.flush();
 		Thread.sleep(5000);
+
 		
-		//Print Hello World
-		terminal.putCharacter('H');
-        terminal.putCharacter('e');
-        terminal.putCharacter('l');
-        terminal.putCharacter('l');
-        terminal.putCharacter('o');
-        terminal.putCharacter('\n');
-        terminal.putCharacter('W');
-        terminal.putCharacter('o');
-        terminal.putCharacter('r');
-        terminal.putCharacter('l');
-        terminal.putCharacter('d');
-        
-        //Flush changes to terminal
-        terminal.flush();
-        
-        Thread.sleep(5000);
+		terminal.flush();
+		
+		
+		
+		
+		
+		
+		
+		
 		}
-		
 		catch(IOException e) 
 		{
 			
@@ -74,4 +68,5 @@ public class masterapp
 
 		}
 	}
+	
 }
