91bb48f12ac410428b086763b27475f360beddff
nbazi1e <narme@10.130.3.201>
2018-06-04 08:50:07 -0400

    Added lines class and first pair of player and AI lines

---
+++ src/lines.java
@@ -0,0 +1,46 @@
+
+public class lines {
+	//All AI lines
+	public static String AIline1 = "Greetings dumpling.";
+	
+	public static String AIline2 = "AI: I said good morning. You were gone for quite awhile.";
+	
+	public static String AIline3 = "You were having your beauty rest weren't you?/nWasting time while the ship crumbles around your worthless soul?";
+		
+	public static String AIline4 = "A case of amnesia mmm? Well, no wonder, it's not like a giant metal beam fell on your head. It's a miracle you're still alive.";
+	
+	public static String AIline5 = "Well, for your information you are on the USS Boaty-McBoatyFace, and you're name is [a worthless waste of my RAM] and for some idiotic reason you decided to take a nap while that spatial anomaly thingy ripped that ship to pieces. Matter of fact, I think we're still stuck in the aforementioned space weirdness, and the hull is liable to disintegrate at any moment.";
+	
+	public static String AIline6 = "Oh them? Hehe. I killed them. They were getting on my nerves. Screaming and whatnot. Stupid creatures.";
+	
+	public static String AIline7v1 = "I want to play with your life! Solve this puzzle if you want to live!";
+	
+	public static String AIline7v2 = "The AI self-destructs blowing you up and killing you because you were \"a pain in the chipset\"";
+	
+	public static String AIline8 = "Okay, how bout you solve this one. You will never!";
+	
+	public static String AIline9 = "How? Well I guess I'm going to die. YAY!";
+	
+	
+	//All player lines
+	public static String playerline1 = "What?";
+	
+	public static String playerline2p1 = "...";
+	
+	public static String playerline2p2 = "What?";
+	
+	public static String playerline3 = "Ship? What ship?";
+	
+	public static String playerline4 = "What about the Captain? The Crew?";
+	
+	public static String playerline5v1 = "...";
+	
+	public static String playerline5v2 = "You Monster! What is wrong with you? Have you no respect for human life?";
+	
+	public static String playerline6 = "Idiot AI!  That was way toooo easy.";
+	
+	public static String playerline7 = "Eureka! I'm a genius. *activates escape pod*";
+	
+	public static String playerline8 = "*escapes from the exploding USS Boaty-McBoatyFace and is rescued*";
+}
+
--- src/masterapp.java
+++ src/masterapp.java
@@ -1,6 +1,7 @@
 //Authors: Narmer Bazile & Suhaas N.
 
 import com.googlecode.lanterna.SGR;
+
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
@@ -9,6 +10,17 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.TerminalResizeListener;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+import java.util.Random;
 
 
 import java.io.IOException;
@@ -21,6 +33,8 @@ public class masterapp
 		//initialize and create terminal window
 		DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
 		Terminal terminal = null;
+		Screen screen = null;
+		
 
 		
 		try 
@@ -28,22 +42,60 @@ public class masterapp
 		
 		//start terminal window
 		terminal = new DefaultTerminalFactory().createTerminal();
+		screen = new TerminalScreen(terminal);
+		screen.startScreen();
+		screen.setCursorPosition(null);
 		
-		final TextGraphics loadMessage = terminal.newTextGraphics();
-		loadMessage.putString(1,1, "Loading ...", SGR.BOLD);
-		terminal.flush();
-		Thread.sleep(5000);
-
+		//create textGraphics object to manipulate textual graphics
+		final TextGraphics txt = screen.newTextGraphics();
 		
-		terminal.flush();
-		
-		
-		
-		
-		
-		
-		
-		
+		//set text color to green on black
+		txt.setForegroundColor(TextColor.ANSI.GREEN);
+        txt.setBackgroundColor(TextColor.ANSI.BLACK);
+        
+        //Print loading screen for 5 secs
+        txt.putString(1,1, "Loading...");
+        screen.refresh();
+        Thread.sleep(5000);
+        
+        
+        //Clear terminal
+        screen.clear();
+        screen.refresh();
+        
+        //Define position and size of answer prompt box 
+        final TerminalPosition boxpos = new TerminalPosition(1,20);
+        final TerminalSize termsize = new TerminalSize(70,3);
+        
+        
+        //Section 1
+        KeyStroke keyStroke = terminal.readInput();
+        
+        while (keyStroke.getKeyType() != KeyType.Escape)
+        {
+	        
+	        txt.putString(1,1,lines.AIline1);
+	        
+	        //Draw answer prompt box
+	        txt.drawRectangle(boxpos, termsize, '-');
+	        txt.putString(3,21, "F1 - " + lines.playerline1);
+	        screen.refresh();
+	
+	        
+	        
+	        keyStroke = terminal.readInput();
+	        
+	        if(keyStroke.getKeyType() == KeyType.F1)
+	        {
+	        	txt.putString(1,2, lines.playerline2p1);
+	        	screen.refresh();
+	        	txt.putString(1,3, lines.playerline2p2);
+	        	Thread.sleep(500);
+	        	screen.refresh();
+	        	
+	        }	
+	        keyStroke = terminal.readInput();
+        }
 		}
 		catch(IOException e) 
 		{
