92a76d18772a0a86d20b3e22bff829be4470c3e1
nbaz1le <narme@10.130.3.201>
2018-06-07 08:27:56 -0400

    Started minigame

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@
 
 import com.googlecode.lanterna.SGR;
 
+
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
@@ -11,18 +12,19 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.TerminalResizeListener;
 import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
+
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 import java.util.Random;
-
-
 import java.io.IOException;
 
 public class test
@@ -31,7 +33,6 @@ public class test
 	public static void main(String[] args) throws InterruptedException 
 	{
 		//initialize and create terminal window
-		DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
 		Terminal terminal = null;
 		Screen screen = null;
 		
@@ -68,7 +69,7 @@ public class test
         final TerminalSize termsize = new TerminalSize(70,3);
         
         KeyStroke keyStroke;
-        
+        int sect5choice = 0;
         while(true) 
         	{
 		            KeyStroke listenForExit = screen.readInput();
@@ -196,9 +197,151 @@ public class test
 				        	screen.refresh();
 
 				        }
+			        	keyStroke = null;
+		    	        txt.putString(45, 20, "Working...");
+		    	        screen.refresh();
+
 			        	
 			        	//Section 5
 			        	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			        	screen.doResizeIfNecessary();
+			        	
+			        	//Draw next AI lines
+			        	txt.putString(1,13, lines.AIline5);
+			        	Thread.sleep(1000);
+			        	screen.refresh();
+			        	
+			        	//Draw answer box
+			        	txt.drawRectangle(boxpos, termsize, '-');
+			        	
+			        	//Draw lines in answer box
+			        	txt.putString(3,21, "F1 - " + lines.playerline5v1 + "     ");
+			        	txt.putString(15,21, "F2 - " + "YOU MONSTER!");
+			        	screen.refresh();
+			        	
+			        	keyStroke = terminal.readInput();
+			        	
+			        	//Wait for input before continuing
+			        	if(keyStroke.getKeyType() == KeyType.F1)
+				        {
+				        	
+				        	txt.putString(1,14,lines.playerline5v1);
+				        	screen.refresh();
+
+				        }
+			        	else if(keyStroke.getKeyType() == KeyType.F2)
+				        {
+				        	
+				        	txt.putString(1,14,lines.playerline5v2);
+				        	sect5choice = 1;
+				        	screen.refresh();
+
+				        }
+			        	keyStroke = null;
+		    	        txt.putString(45, 20, "Working...");
+		    	        screen.refresh();
+
+		    	        //Section 6
+			        	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			        	screen.doResizeIfNecessary();
+			        	
+		    	        //Set intimidating color for AI lines
+			        	txt.setForegroundColor(TextColor.ANSI.WHITE);
+			        	txt.setBackgroundColor(TextColor.ANSI.RED);
+			        	
+			        	//Death scene
+			        	if(sect5choice == 1) 
+			        	{
+			        		//print death message
+			        		txt.putString(1,15, lines.AIline6v2,SGR.BLINK);
+			        		screen.refresh();
+			        		
+			        		for(int i = 0; i < 3; i++)
+			        			terminal.bell();
+			        		
+			        		Thread.sleep(3000);
+			        		
+			        		//Use random to draw random colors on terminal
+			        		Random random = new Random();
+			        		
+			        		//Get size of terminal
+			                TerminalSize terminalSize = screen.getTerminalSize();
+			                
+			                //loop through all terminal row cells
+			                for(int row = 0; row < terminalSize.getRows(); row++) 
+			                {
+			                	//loop through all terminal columns cells
+			                    for(int column = 0; column < terminalSize.getColumns(); column++) 
+			                    {
+			                    	//set cell to random color
+			                        screen.setCharacter(column, row, new TextCharacter(
+			                                ' ',
+			                                TextColor.ANSI.DEFAULT,
+			                                // This will pick a random background color
+			                                TextColor.ANSI.values()[random.nextInt(TextColor.ANSI.values().length)]));
+			                        
+			                        //wait 1 millisecond
+			                        Thread.sleep(1);
+			                        
+			                        //update video buffer
+			                        screen.refresh();
+			                        
+			                        //same as above
+			                        screen.setCharacter(row, column, new TextCharacter(
+			                                ' ',
+			                                TextColor.ANSI.DEFAULT,
+			                                // This will pick a random background color
+			                                TextColor.ANSI.values()[random.nextInt(TextColor.ANSI.values().length)]));
+			                        screen.refresh();
+			                        Thread.sleep(1);
+			                        
+			                    }
+			                }
+			                
+			                screen.refresh();
+			        	}
+			        	
+			        	//You made the right choice and lived
+			        	txt.putString(1, 15, lines.AIline6v1);  
+			        	screen.refresh();
+			        	Thread.sleep(5000);
+			        	
+			        	//First minigame
+			        	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			        			        	
+			        	screen.clear();
+			        	
+			        	txt.setForegroundColor(TextColor.ANSI.WHITE);
+			        	txt.setBackgroundColor(TextColor.ANSI.BLACK);
+			        	
+//			        	//draw tic tac toe board
+//			        	//draw vertical lines
+//			        	txt.drawLine(3, 1, 3, 5, '|');
+//			        	txt.drawLine(8, 1, 8, 5, '|');
+//			        	//draw hrizontol lines
+//			        	txt.drawLine(1, 1, 1, 5, '-');
+//			        	txt.drawLine(6, 1, 6, 5, '-');
+//			        	txt.drawLine(11, 1, 11, 5, '-');
+//			        	screen.refresh();
+//			        	Thread.sleep(5000);
+			        	
+			        	final TerminalPosition hackbox1pos = new TerminalPosition(1, 5);
+			        	final TerminalPosition hackbox2pos = new TerminalPosition(26, 5);
+			        	final TerminalPosition hackbox3pos = new TerminalPosition(51, 5);
+			        	final TerminalPosition hackbox4pos = new TerminalPosition(76, 5);
+			        	
+			            final TerminalSize hackboxsize = new TerminalSize(20,10);
+			            
+			           txt.fillRectangle(hackbox1pos, hackboxsize, new TextCharacter(' ', TextColor.ANSI.DEFAULT, TextColor.ANSI.GREEN));
+			           txt.fillRectangle(hackbox2pos, hackboxsize, new TextCharacter(' ', TextColor.ANSI.DEFAULT, TextColor.ANSI.RED));
+			           txt.fillRectangle(hackbox3pos, hackboxsize, new TextCharacter(' ', TextColor.ANSI.DEFAULT, TextColor.ANSI.MAGENTA));
+			           txt.fillRectangle(hackbox4pos, hackboxsize, new TextCharacter(' ', TextColor.ANSI.DEFAULT, TextColor.ANSI.YELLOW));
+			           
+
+			           
+			           
+			           screen.refresh();
+			           Thread.sleep(10000);
 			        	
 			        	
 			        	
@@ -211,7 +354,16 @@ public class test
 			        	
 			        	
 			        	
-			     
+			        	
+			        	
+			        	
+			        	
+			        	
+			        	
+			        	
+			        	
+			        	
+			        	break;
 	           }
         	   Thread.yield();
         	   screen.close();
